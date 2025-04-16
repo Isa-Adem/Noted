6b34ead9e7637cf3d5891cc5272e753de4678da8
Isa-Adem <isa.adem82@gmail.com>
2025-04-16 07:46:28 -0400

    Initial Commit

---
+++ NotedTests/NotedTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotedTests.swift
+//  NotedTests
+//
+//  Created by isa adem on 4/16/25.
+//
+
+import Testing
+@testable import Noted
+
+struct NotedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
